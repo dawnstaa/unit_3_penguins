8752e660f0b6cb46ee4f6879748977f71978d48a
dawnstaa <staamadi@seoe.sc.edu>
2022-02-15 15:01:44 -0500

    first try

---
+++ tidyverse_intro_1.R
@@ -0,0 +1,11 @@
+#Amadi 02/15/2022 with Erin on Tidyverse 
+install.packages("tidyverse")
+library("tidyverse")
+tidyverse_packages()
+
+
+
+
+my_data = as.data.frame(my_data)
+library(palmerpenguins)
+head(penguins)
